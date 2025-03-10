b2450c6008b2a0bbb07f0c6bd1fec3246b94fe80
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-10 14:05:22 +0700

    ADD: Added service add

---
+++ app/addService.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function addService() {
+  return (
+    <View>
+      <Text>addService</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
