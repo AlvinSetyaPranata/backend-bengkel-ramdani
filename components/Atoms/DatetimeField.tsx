69d16ccceaebd10c9211759dc148b51fe954816d
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-11 14:02:45 +0700

    UPDATE: Fixed issue on datetime when dismiss or setting

--- components/Atoms/DatetimeField.tsx
+++ components/Atoms/DatetimeField.tsx
@@ -1,80 +1,89 @@
-import { KeyboardTypeOptions, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { MaterialIcons } from '@expo/vector-icons'
-import RNDateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker'
-
+import {
+  KeyboardTypeOptions,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import RNDateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
 
 interface Props {
-  title: string,
-  name: string,
-  setter: (name: string, value: string) => void,
-  defaultValue?: string,
+  title: string;
+  name: string;
+  setter: (name: string, value: string) => void;
+  defaultValue?: string;
 }
 
-
-export default function DatetimeField({ title, name, setter, defaultValue="" }: Props) {
-
-  const [value, setValue] = useState(new Date())
-  const [show, setShow] = useState(false)
+export default function DatetimeField({
+  title,
+  name,
+  setter,
+  defaultValue = "",
+}: Props) {
+  const [value, setValue] = useState(new Date());
+  const [show, setShow] = useState(false);
 
   const onChangeHandler = (event: DateTimePickerEvent, selectedDate?: Date) => {
-    event.nativeEvent
+    event.nativeEvent;
 
     if (event.type == "dismissed" || event.type == "set") {
-      setShow(false)
-      return
-    }
+      if (selectedDate) {
+        setValue(selectedDate);
+        setter(name, selectedDate);
+      }
 
-    if (selectedDate) {
-      setValue(selectedDate.getFullYear())
-      setter(name, selectedDate)
+      setShow(false);
+      return;
     }
-
-    setShow(false)
-    
-  }
+  };
 
   const styles = StyleSheet.create({
-
     inputContainer: {
-      width: '100%',
-      flexDirection: 'row',
-      alignItems: 'center',
-      backgroundColor: 'white',
+      width: "100%",
+      flexDirection: "row",
+      alignItems: "center",
+      backgroundColor: "white",
       borderRadius: 10,
       marginTop: 12,
       paddingHorizontal: 10,
     },
-  
+
     input: {
-      flexBasis: '0%',
+      flexBasis: "0%",
       flexGrow: 1,
       flexShrink: 1,
     },
-  
+
     errorMessege: {
-      color: 'red',
+      color: "red",
       fontWeight: 500,
       fontSize: 12,
       marginTop: 10,
-      marginLeft: 10
-    }
-  })
+      marginLeft: 10,
+    },
+  });
 
-  useEffect(() => console.log(value), [value])
+  useEffect(() => console.log(value), [value]);
 
   return (
     <View>
       <Text>{title}</Text>
       <View style={styles.inputContainer}>
-        <TextInput readOnly={true} value={value.getFullYear().toString()}  style={styles.input} />
+        <TextInput
+          readOnly={true}
+          value={value.getFullYear().toString()}
+          style={styles.input}
+        />
         <Pressable onPress={() => setShow(true)}>
           <MaterialIcons name="date-range" color="black" size={16} />
         </Pressable>
       </View>
-      {show && <RNDateTimePicker value={new Date()} onChange={onChangeHandler} />
-      }
+      {show && <RNDateTimePicker value={value} onChange={onChangeHandler} />}
     </View>
-  )
+  );
 }
-
